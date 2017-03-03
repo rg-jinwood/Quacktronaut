109f23c50e8c151725d37f092a77bc6a3a5a5f51
Julian <jinwood8@outlook.com>
2017-03-03 19:42:34 +0000

    Add main menu with elements

---
+++ Assets/Scripts/Menu/PlanetFloat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetFloat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0.07f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0, -1 * Time.deltaTime);
+    }
+}
