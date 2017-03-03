a3d0eedf6b7df75a4e6043c2b362b95d384c9b72
Julian <jinwood8@outlook.com>
2017-03-03 07:48:06 +0000

    Add some randomness to object spawner

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -1,30 +1,47 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour {
 
     public Vector3 SpawnLocation;
     public List<GameObject> SpawnableObjects;
+    bool isSpawning = false;
+    public float minTime = 1.0f;
+    public float maxTime = 3.0f;
 
     private Random random = new Random();
 
-	void FixedUpdate ()
+    private void Update()
     {
-        var obj = SpawnableObjects[Random.Range(0, SpawnableObjects.Count)];
-
-        if (Spawnable())
+        if (!isSpawning)
         {
-            SpawnObject();
+            isSpawning = true;
+            int enemyIndex = Random.Range(0, SpawnableObjects.Count);
+            StartCoroutine(SpawnObject(enemyIndex, Random.Range(minTime, maxTime)));
+            //InvokeRepeating("SpawnObject", 1, 1);}
         }
     }
 
     private bool Spawnable()
     {
+        var x = GameObject.FindGameObjectsWithTag("Obstacle");
         return GameObject.FindGameObjectsWithTag("Obstacle").Length < 3;
     }
 
-    private void SpawnObject()
+    private IEnumerator SpawnObject(int index, float seconds)
     {
+            if (Spawnable())
+            {
+                Debug.Log("Waiting for " + seconds + " seconds");
+
+                yield return new WaitForSeconds(seconds);
+                var obj = SpawnableObjects[Random.Range(0, SpawnableObjects.Count)];
+                Instantiate(obj, SpawnLocation, Quaternion.identity);
+
+                //We've spawned, so now we could start another spawn     
+                isSpawning = false;
+            } 
         //combine spawnable objects from SpawnableObjects
         //with whatever is fetched from API
     }
